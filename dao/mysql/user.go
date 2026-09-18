19c5b5ee42bb5e870f56c68ea504be29fc45dae0
捷 <925394879@qq.com>
2026-09-18 15:10:25 +0800

    完成数据库插入和密码加密

--- dao/mysql/user.go
+++ dao/mysql/user.go
@@ -1,5 +1,12 @@
 package mysql
 
+import (
+	"bluebell/models"
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// CheckUserExist 检查指定用户名的用户是否存在
 func CheckUserExist(username string) (bool, error) {
 	sqlStr := `select count(user_id) from user where username=?`
 	var count int
@@ -10,6 +17,20 @@ func CheckUserExist(username string) (bool, error) {
 	return count > 0, nil
 }
 
-func InsertUser() {
+// InsertUser 向数据库中插入新用户
+func InsertUser(user *models.User) (err error) {
+	//对密码进行加密
+	user.Password = encryptPassword(user.Password)
+	//执行sql语句入库
+	sqlStr := `insert into user (user_id,username,password) values(?,?,?)`
+	_, err = db.Exec(sqlStr, user.UserID, user.Username, user.Password)
+	return
+}
 
+const secret = "xiaohuang"
+
+func encryptPassword(oPassword string) string {
+	h := md5.New()
+	h.Write([]byte(secret))
+	return hex.EncodeToString(h.Sum([]byte(oPassword)))
 }
